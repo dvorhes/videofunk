91635bd590a9552b5d608d622ad654b54f1a55e3
Davidson Vorhes <dvorhes@gmail.com>
2022-09-24 15:33:30 -0400

    working extract function

--- functions/extract.py
+++ functions/extract.py
@@ -1,5 +1,7 @@
+import pathlib
 from pathlib import Path
-
+import sys
+import subprocess
 
 def extract(input_path : str | pathlib.PosixPath,
                  time_in : int | float,
@@ -10,12 +12,27 @@ def extract(input_path : str | pathlib.PosixPath,
         string_return = True
         input_path = Path(input_path)
     if not output_path:
-        output_path = Path.cwd()
+        output_path = input_path.with_name(f"{input_path.stem}_{time_in}_{time_out}{input_path.suffix}")
     else:
-        output_path = Path(output_path)
+        output_path = Path(output_path) 
 
     cmd = ["ffmpeg", "-ss", f"{time_in}", "-i", f"{input_path}",
            "-to", f"{time_out}", f"{output_path}"]
+
+    for i in cmd:
+        print(i)
+
     subprocess.call(cmd)
 
     return str(output_path) if string_return else output_path
+
+
+if __name__ == "__main__":
+    try:
+        input_path = sys.argv[1]
+        time_in = sys.argv[2]
+        time_out = sys.argv[3]
+    except IndexError:
+        print("\n\nError. Must have input_path, time_in, and time_out args\n\n")
+
+    extract(input_path, time_in, time_out)
